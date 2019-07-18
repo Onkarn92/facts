1cfecbc157ed0564cf4b0a1fd39f9cad4ea92157
Onkar Nene <onkar.nene@effective.digital>
2019-07-18 12:16:42 +0530

    #3 Added fact list item layout and adapter for smooth scrolling.

--- app/src/main/java/com/onkarnene/android/facts/adapters/FactListAdapter.kt
+++ app/src/main/java/com/onkarnene/android/facts/adapters/FactListAdapter.kt
@@ -6,4 +6,85 @@
 
 package com.onkarnene.android.facts.adapters
 
-class FactListAdapter {}
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.onkarnene.android.facts.R
+import com.onkarnene.android.facts.adapters.FactListAdapter.ViewHolder
+import com.onkarnene.android.facts.models.Fact
+import com.onkarnene.android.facts.utilities.NA
+import kotlinx.android.synthetic.main.item_fact_list.view.*
+
+/**
+ * Responsible for holding and recycling the fact items in a list.
+ */
+class FactListAdapter(private val callback: Callback) : RecyclerView.Adapter<ViewHolder>() {
+	
+	private val items = mutableListOf<Fact>()
+	
+	/**
+	 * Notifies the adapter to load the list again.
+	 *
+	 * @param facts to be added in list.
+	 */
+	fun setItems(facts: ArrayList<Fact>) {
+		facts.removeAll {
+			it.title.isNullOrBlank() && it.description.isNullOrBlank() && it.imageHref.isNullOrBlank()
+		}
+		for (item in facts) {
+			if (!items.contains(item)) {
+				items.add(item)
+			}
+		}
+		notifyDataSetChanged()
+	}
+	
+	override fun onCreateViewHolder(
+			parent: ViewGroup,
+			viewType: Int
+	): ViewHolder {
+		val holder = ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_fact_list, parent, false))
+		holder.itemView.setOnClickListener {callback.onItemClick(holder.fact)}
+		return holder
+	}
+	
+	override fun getItemCount(): Int = items.size
+	
+	override fun onBindViewHolder(
+			holder: ViewHolder,
+			position: Int
+	) {
+		holder.setData(items[position])
+	}
+	
+	/**
+	 * Responsible for holding item related data.
+	 */
+	class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+		
+		lateinit var fact: Fact
+		
+		/**
+		 * Inject respective [Fact] data in layout.
+		 *
+		 * @param fact to be loaded in layout.
+		 */
+		fun setData(fact: Fact) {
+			this.fact = fact
+			this.itemView.factTitleText.text = this.fact.title ?: NA
+			this.itemView.factDescriptionText.text = this.fact.description ?: NA
+			Glide.with(this.itemView.factImage).load(this.fact.imageHref).centerCrop().placeholder(R.drawable.loading_placeholder)
+					.error(R.drawable.error_placeholder).into(this.itemView.factImage)
+		}
+	}
+	
+	interface Callback {
+		
+		/**
+		 * Triggers when specific list item gets click event.
+		 */
+		fun onItemClick(fact: Fact)
+	}
+}
